9b9036a4d43700c629e58c00ec0fec6c6cc51e80
LiuHuipeng16 <1229035387@qq.com>
2026-04-09 03:30:23 +0800

    补充model文件夹

---
+++ agri-warning-platform/src/main/java/com/zhku/agriwarningplatform/module/ai/controller/AIController.java
@@ -0,0 +1,15 @@
+package com.zhku.agriwarningplatform.module.ai.controller;
+
+import jdk.jfr.Registered;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 12290
+ * Date: 2026-04-09
+ * Time: 3:28
+ */
+@RestController
+public class AIController {
+}
